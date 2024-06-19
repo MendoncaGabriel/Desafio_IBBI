c156861b5bd3eeb8d0b608418704bb3ce914779d
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-06-19 11:58:28 -0400

    ajuste no relacionamento entre produto e categoria

--- src/models/categoria.py
+++ src/models/categoria.py
@@ -1,8 +1,12 @@
 from sqlalchemy import Column, Integer, String
 from src.config.database import Base
+from sqlalchemy.orm import relationship
 
 class Categoria(Base):
     __tablename__ = "categorias"
     
     id = Column(Integer, primary_key=True, index=True)
     descricao = Column(String(255), index=True)
+
+    # Relacionamento com produtos
+    produtos = relationship("Produto", back_populates="categoria")
--- src/models/produto.py
+++ src/models/produto.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from src.config.database import Base
 
 class Produto(Base):
@@ -8,3 +9,7 @@ class Produto(Base):
     nome = Column(String(255), index=True)
     descricao = Column(String(255), index=True)
     preco = Column(Float, index=True)
+    categoria_id = Column(Integer, ForeignKey("categorias.id"))
+
+    # Relacionamento com categoria
+    categoria = relationship("Categoria", back_populates="produtos")
